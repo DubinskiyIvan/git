269583e7bf550103db8f5846de50d098ff133fdc
Dubisnkiy Ivan <dubinskiy_ib@mail.ru>
2020-11-12 18:09:30 +0700

    ab

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+double my_pow(double x, unsigned int y) {
+    double c = x;
+    for (int i = 1; i < y; i++)
+        c *= x;
+    return c;
+}
+
+int main() {
+    setlocale(LC_ALL, "russian");
+    double x;
+    unsigned int y;
+    cout << "Введите число x: ";
+    cin >> x;
+    cout << "Введите степень y: ";
+    cin >> y;
+    cout << endl;
+    cout << "Результат: " << my_pow(x, y) << endl;
+    system("pause");
+    return 0;
+}
